960f74b43ccc2ed76aee8cde743f3944feaa2952
Thibault Garçon <thibault.garcon@pole-emploi.fr>
2020-10-03 17:54:31 +0200

    add doc

--- pytodo/__pytodo__.py
+++ pytodo/__pytodo__.py
@@ -1,5 +1,43 @@
 from pathlib import Path
 
+def help():
+	help_txt = """\
+# pytodo
+
+pytodo is a todolist programm who works with a .todo file at the root of the home directory
+
+## Install
+
+```
+git clone https://github.com/thib1984/pytodo.git
+cd pytodo
+pip3 install --user .
+```
+or directly
+```
+pip3 install --user git+https://github.com/thib1984/pytodo.git#egg=pytodo
+```
+
+## Utilisation
+
+```
+pytodo-add "to do work #name_of_tag" #add the task 'to do work' with the tag 'name_of_tag'
+pytodo-add "to do other_work #name_of_other_tag" #add the other task 'to do other_work' with the tag 'name_of_other_tag'
+pytodo-add "to do other_big_work #name_of_other_tag" #add the other task 'to do other_big_work' with the tag 'name_of_tag'
+pytodo-list #print the todolist with an index for each task
+pytodo-list "name_of_tag" #print the todolist filtered to the tag name_of_tag
+pytodo-del 2 #delete the second task of the todolist
+pytodo-clear #reorder the todolist in consecutives index
+```
+
+## Uninstall
+
+```
+pip3 uninstall pytodo
+```
+	"""
+	print(help_txt)
+
 def list():
     check()
     import sys
@@ -72,3 +110,5 @@ if __name__ == "__delete__":
     delete()
 if __name__ == "__clear__":
     clear()    
+if __name__ == "__help__":
+    help()
--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 setup(name='pytodo',
       version='0.1.0',
-      description='pytodo',
+      description='pytodo is a todolist program who works with a .todo file at the root of the home directory',
       url='https://github.com/thib1984/pytdodo',
       author='thib1984',
       author_email='thibault.garcon@gmail.com',
-      license='MIT',
       packages=['pytodo'],
       zip_safe=False,
       entry_points = {
-          'console_scripts': ['pytodo-list=pytodo.__pytodo__:list','pytodo-add=pytodo.__pytodo__:add','pytodo-del=pytodo.__pytodo__:delete','pytodo-clear=pytodo.__pytodo__:clear'],
+          'console_scripts': ['pytodo-list=pytodo.__pytodo__:list','pytodo-add=pytodo.__pytodo__:add','pytodo-del=pytodo.__pytodo__:delete','pytodo-clear=pytodo.__pytodo__:clear','pytodo=pytodo.__pytodo__:help'],
       }
 )
