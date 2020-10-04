1f45b67275dd1b20206e16830228b9ce968cd917
Thibault Garçon <thibault.garcon@pole-emploi.fr>
2020-10-04 19:52:44 +0200

    rename package

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='pytodolist',
+setup(name='pypodo',
       version='0.2.0',
-      description='pytodolist is a todolist program who works with a .todo file at the root of the home directory : use pytodo to run it',
-      url='https://github.com/thib1984/pytdodo',
+      description='pypodo is a todolist program who works with a .todo file at the root of the home directory : use pypodo to run it',
+      url='https://github.com/thib1984/pypodo',
       author='thib1984',
       author_email='thibault.garcon@gmail.com',
-      packages=['pytodolist'],
+      packages=['pypodo'],
       zip_safe=False,
       entry_points = {
-          'console_scripts': ['pytodo=pytodolist.__pytodolist__:pytodo'],
+          'console_scripts': ['pypodo=pypodo.__pypodo__:pypodo'],
       }
 )
