e690f209263edd4eea1362993fad3388de1c864c
thib1984 <45128847+thib1984@users.noreply.github.com>
2020-10-04 11:47:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pytodolist',
       version='0.2.0',
-      description='pytodo is a todolist program who works with a .todo file at the root of the home directory',
+      description='pytodolist is a todolist program who works with a .todo file at the root of the home directory : use pytodo to run',
       url='https://github.com/thib1984/pytdodo',
       author='thib1984',
       author_email='thibault.garcon@gmail.com',
