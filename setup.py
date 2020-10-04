06d076c4b3f4c1047d4c2a2320a6453a2cfda8ba
Thibault Garçon <thibault.garcon@pole-emploi.fr>
2020-10-04 11:45:02 +0200

    reame package

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name='pytodo',
+setup(name='pytodolist',
       version='0.2.0',
       description='pytodo is a todolist program who works with a .todo file at the root of the home directory',
       url='https://github.com/thib1984/pytdodo',
       author='thib1984',
       author_email='thibault.garcon@gmail.com',
-      packages=['pytodo'],
+      packages=['pytodolist'],
       zip_safe=False,
       entry_points = {
-          'console_scripts': ['pytodo=pytodo.__pytodo__:pytodo'],
+          'console_scripts': ['pytodo=pytodolist.__pytodolist__:pytodo'],
       }
 )
