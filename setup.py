4f921d0ebdfb8f961ef3d2ebfe23b2a9eecb2728
thib1984 <45128847+thib1984@users.noreply.github.com>
2020-10-06 09:27:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pypodo',
-      version='0.2.0',
+      version='0.3.0',
       description='pypodo is a todolist program who works with a .todo file at the root of the home directory : use pypodo to run it',
       url='https://github.com/thib1984/pypodo',
       author='thib1984',
