a57eec4adbf3cc882211f78ab71f800faa59c70e
Thibault Garçon <thibault.garcon@pole-emploi.fr>
2020-10-13 21:49:21 +0200

    maj action

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pypodo',
       version='2.0.0',
       description='pypodo is a todolist tool which works with a .todo file positionned the root of the home directory : use pypodo to run it.',
