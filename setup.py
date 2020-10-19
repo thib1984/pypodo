5593fcc1d3be6d67f7dc65ab274b773ba419b847
Thibault GARCON <thibault.garcon@gmail.com>
2020-10-19 22:17:42 +0200

    correction setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(name='pypodo',
       version='2.1.1',
-      description='pypodo is a todolist tool which works with a .todo file positionned the root of the home directory : use pypodo to run it. Pypodo has mecanisms
- of index and tags',
+      description='pypodo is a todolist tool which works with a .todo file positionned the root of the home directory : use pypodo to run it. Pypodo has mecanisms of index and tags',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='https://github.com/thib1984/pypodo',
