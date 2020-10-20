0e7572fe53de122d3f3187266472ae28943b6e21
Thibault GARCON <thibault.garcon@gmail.com>
2020-10-20 21:00:17 +0200

    montee version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pypodo',
-      version='2.1.1',
+      version='2.2.0',
       description='pypodo is a todolist tool which works with a .todo file positionned the root of the home directory : use pypodo to run it. Pypodo has mecanisms of index and tags',
       long_description=long_description,
       long_description_content_type="text/markdown",
