211a13695f9828636428f707a54d4a24f4570c28
Thibault GARCON <thibault.garcon@gmail.com>
2021-11-27 12:14:46 +0100

    CHORE : version5

--- setup.py
+++ setup.py
@@ -14,9 +14,19 @@ setup(
     author_email="thibault.garcon@gmail.com",
     license="mit",
     packages=["pypodo"],
-    install_requires=["termcolor","columnar"],
+    install_requires=["termcolor","columnar","colorama"],
     zip_safe=False,
     entry_points={
         "console_scripts": ["pypodo=pypodo.__init__:pypodo"],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
