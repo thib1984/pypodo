099f46ba88c0d9a2acc0598f2c1cc6f8b248b58e
WolfBlack <denizot.myriam@gmail.com>
2026-02-12 10:08:38 +0100

    fix(version): changement de version pour python 3.14 avec condition

--- pypodo/version.py
+++ pypodo/version.py
@@ -1,4 +1,10 @@
-import pkg_resources
+try:
+    from importlib.metadata import version as get_version
+except ImportError:
+    import pkg_resources
+    def get_version(package_name):
+        return pkg_resources.get_distribution(package_name).version
+
 import os
 from pypodo.properties import TODO_RC_FILE
 from pypodo.config import get_user_config_directory_pypodo, todofilefromconfig, todobackupfolderfromconfig
@@ -7,9 +13,7 @@ def version():
     """
     entry point for --version
     """
-    print("version pypodo            : "
-        + pkg_resources.get_distribution("pypodo").version
-    )
+    print("version pypodo            : " + get_version("pypodo"))
     print("location of todo file     : " + todofilefromconfig())
     print("location of config file   : " + os.path.join(get_user_config_directory_pypodo(), TODO_RC_FILE))
     print("location of backup folder : " + todobackupfolderfromconfig())
